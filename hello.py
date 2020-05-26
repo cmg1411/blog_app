406337d2e038248057677889b70c10c0a57b8f22
mingoer <cmg14111@gmail.com>
2020-05-26 09:43:45 +0900

    pipenv, kappa

--- hello.py
+++ hello.py
@@ -1,11 +1,12 @@
-from sanic import Sanic
-from sanic.response import json
+from flask import Flask
 
-app = Sanic()
+app = Flask(__name__)
+
 
 @app.route('/')
-async def test(request):
-    return json({'hello': 'world'})
+def index():
+    return 'won uk park'
+
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000)
+    app.run()
