92c638e02ac68ba2f6e2427b875ac4048bddafee
mingoer <cmg14111@gmail.com>
2020-05-26 11:26:49 +0900

    zappa json파일에 project-name 삭제. aws의 access key 바로 참조할 수 있게.

--- hello.py
+++ hello.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'geolife is so hard ... '
+    return 'geolife is so hard ... i need something to rest...'
 
 
 if __name__ == '__main__':
